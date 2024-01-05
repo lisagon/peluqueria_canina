5dc8e5bce570d8077f7be04982e51ae39b8b553b
lisa <katzepin@gmail.com>
2024-01-05 19:53:47 -0300

    [NEW] first test commit

---
+++ peluqueria_canina/src/peluqueria_canina/Peluqueria_canina.java
@@ -0,0 +1,12 @@
+
+package peluqueria_canina;
+
+
+public class Peluqueria_canina {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+    
+}

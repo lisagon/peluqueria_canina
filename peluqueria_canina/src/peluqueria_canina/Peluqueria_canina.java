315c49e5edce4b7b762ad7968eda03e7da5d4213
lisa <katzepin@gmail.com>
2024-01-08 15:41:54 -0300

    [NEW] basic interface created

--- peluqueria_canina/src/peluqueria_canina/Peluqueria_canina.java
+++ peluqueria_canina/src/peluqueria_canina/Peluqueria_canina.java
@@ -1,12 +1,19 @@
 
 package peluqueria_canina;
 
+import igu.App;
+
 
 public class Peluqueria_canina {
 
     
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        
+        App first_screen = new App();
+        first_screen.setVisible(true);
+        first_screen.setLocationRelativeTo(null);
+        
+        
     }
     
 }
